df9f4290d8b4ec7523fc48b61ab3c6822332bf2b
Arwichok <arwichok@gmail.com>
2023-12-15 03:00:29 +0200

    dockerized

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,26 @@
 from litestar import Litestar, get
 import msgspec
+import faker
 
 
-class Data(msgspec.Struct):
+fake = faker.Faker()
+
+
+class User(msgspec.Struct):
     name: str
     age: int
 
 
+class Data(msgspec.Struct):
+    user: User
+    text: str
+
+
+
 @get("/")
-async def hello() -> dict:
-    return Data(name="John", age=42)
+async def hello() -> list[Data]:
+    return [Data(user=User(name=fake.name(), age=fake.random_int(min=1, max=100)), text=fake.text()) for _ in range(4)]
+
+
 
 app = Litestar(route_handlers=[hello])
